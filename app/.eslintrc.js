16c0a2e62a0f1a6d22a24590b0b44fc9010183cb
tolujay <fatexjames@yahoo.com>
2025-04-22 16:55:20 -0400

    FEAT(#1): Adding initial babel and linter configurations

---
+++ app/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/typescript/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		"@typescript-eslint/no-explicit-any": "warn",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"vue/multi-word-component-names": "off",
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)",
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
+}
