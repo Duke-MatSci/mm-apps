ffc157a5c7400893e2201b946dd7c7651af21c43
tolujay <fatexjames@yahoo.com>
2025-04-22 17:56:16 -0400

    FEAT(#1): Modifying linter to support vue parser

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		"plugin:vue/vue3-essential",
-		"eslint:recommended",
-		"@vue/typescript/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
-	],
-	parserOptions: {
-		ecmaVersion: 2020,
-	},
-	rules: {
-		"@typescript-eslint/no-explicit-any": "warn",
-		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"vue/multi-word-component-names": "off",
-		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-	},
-	overrides: [
-		{
-			files: [
-				"**/__tests__/*.{j,t}s?(x)",
-				"**/tests/unit/**/*.spec.{j,t}s?(x)",
-			],
-			env: {
-				jest: true,
-			},
-		},
-	],
-}
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
